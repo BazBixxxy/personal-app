38618d94e729e8f0201d4103f670a24885bec898
BazBixxxy <133676922+BazBixxxy@users.noreply.github.com>
2025-09-05 19:52:55 +0300

    `Refactor: Replaced AboutMe component with a container div and adjusted layout`

--- src/app/home/pages/HomePage.jsx
+++ src/app/home/pages/HomePage.jsx
@@ -6,7 +6,11 @@ const HomePage = () => {
   return (
     <main className="p-2 max-w-screen-xl mx-auto font-mono font-medium text-lg">
       <Typewriter />
-      <AboutMe />
+      <div className="w-full lg:flex justify-between">
+        <div className="p-3 w-full xl:w-2/3 2xl:w-7/12 flex flex-col gap-4">
+          <AboutMe />
+        </div>
+      </div>
     </main>
   );
 };
