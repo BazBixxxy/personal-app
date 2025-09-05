9c074da3b654ecdda3a2f58b1390bda6ae6c1c29
BazBixxxy <133676922+BazBixxxy@users.noreply.github.com>
2025-09-05 19:15:39 +0300

    `Removed all components and pages from the project`

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,9 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
-import QueryPage from "./pages/QueryPage";
-import BooksPage from "./pages/BooksPage";
-import NewsPage from "./pages/NewsPage";
-import FormsPage from "./pages/FormsPage";
-import NewFormPage from "./pages/NewFormsPage";
-import Maps from "./pages/Maps";
 
 const App = () => {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route path="/" element={<QueryPage />} />
-        <Route path="/books" element={<BooksPage />} />
-        <Route path="/news" element={<NewsPage />} />
-        <Route path="/forms" element={<FormsPage />} />
-        <Route path="/new-form" element={<NewFormPage />} />
-        <Route path="/maps" element={<Maps />} />
-      </>
-    )
-  );
+  const router = createBrowserRouter(createRoutesFromElements(<></>));
 
   return <RouterProvider router={router} />;
 };
