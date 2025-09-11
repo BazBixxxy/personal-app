8d8382106cc5c805596be103e7b7f220fe4b0f54
BazBixxxy <133676922+BazBixxxy@users.noreply.github.com>
2025-09-11 18:54:51 +0300

    `Refactor Hero component: update height, path position, and remove footer section`

--- src/app/home/components/hero.jsx
+++ src/app/home/components/hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
   return (
     <section
       ref={ref}
-      className="mx-auto flex h-[350vh] w-screen flex-col items-center overflow-hidden px-4 pt-20"
+      className="mx-auto flex h-[250vh] w-screen flex-col items-center overflow-hidden px-4 pt-20"
     >
       <div className="mt-42 relative flex w-fit flex-col items-center justify-center gap-5 text-center text-[#1F3A4B] dark:text-primary">
         <h1 className="relative z-10 text-7xl font-medium tracking-[-0.08em] lg:text-9xl">
@@ -22,36 +22,10 @@ const Hero = () => {
           us. And we ought to lay down our lives for our brothers and sisters."
           1 John 3:16
         </p>
-
         <LinePath
-          className="absolute -right-[40%] top-0 z-0"
+          className="absolute -right-[20%] -top-40 z-0"
           scrollYProgress={scrollYProgress}
         />
-      </div>
-
-      <div className="rounded-4xl w-full translate-y-[100vh] bg-[#1F3A4B] pb-10 text-[#FAFDEE]">
-        <h1 className="mt-10 text-center text-[15.5vw] font-bold leading-[0.9] tracking-tighter lg:text-[16.6vw]">
-          skiperui.com
-        </h1>
-        <div className="mt-80 flex w-full flex-col items-start gap-5 px-4 font-medium lg:mt-0 lg:flex-row lg:justify-between">
-          <div className="flex w-full items-center justify-between gap-12 uppercase lg:w-fit lg:justify-center">
-            <p className="w-fit text-sm">
-              punjab, india <br />
-              and online
-            </p>
-            <p className="w-fit text-right text-sm lg:text-left">
-              sep 1, 2025 <br /> the Moosa pind
-            </p>
-          </div>
-          <div className="flex w-full flex-wrap items-center justify-between gap-12 uppercase lg:w-fit lg:justify-center">
-            <p className="w-fit text-sm">
-              onilne <br /> free
-            </p>
-            <p className="w-fit text-right text-sm lg:text-left">
-              in person tickets <br /> $600
-            </p>
-          </div>
-        </div>
       </div>
     </section>
   );
