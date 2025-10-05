b7d05fbb83805376763108f4d1990c7735a191ef
BazBixxxy <133676922+BazBixxxy@users.noreply.github.com>
2025-10-05 22:24:09 +0300

    `Removed unnecessary className attributes from HomePage component`

--- src/app/main/home/pages/HomePage.jsx
+++ src/app/main/home/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import Hero from "../components/hero";
 
 const HomePage = () => {
   return (
-    <main className="p-2 max-w-screen-xl mx-auto font-mono font-medium text-lg pb-20">
+    <main>
       <Hero />
     </main>
   );
