e79412af830b454423ea88b7642bf4666a4203e5
BazBixxxy <133676922+BazBixxxy@users.noreply.github.com>
2025-10-17 21:39:08 +0300

    Sure! Please provide the git diff so I can help you craft an appropriate commit message.

---
+++ src/components/login/login-form.jsx
@@ -0,0 +1,179 @@
+import { Eye, Loader2 } from "lucide-react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import GoogleAuth from "./google-auth";
+import { useSearchParams } from "react-router-dom";
+import { loginSchema } from "@/lib/schema";
+import { Link } from "react-router-dom";
+import { EyeOffIcon } from "lucide-react";
+import Logo from "@/components/logo/logo";
+import TermsAndConditions from "@/components/policies/terms-and-conditions";
+import PrivacyPolicies from "@/components/policies/privacy-policies";
+import useLogin from "@/hooks/auth/useLogin";
+
+export function LoginForm({ className, ...props }) {
+  const { loading, login } = useLogin();
+  const [searchParams] = useSearchParams();
+  const tempPassword = searchParams.get("temp-password");
+  // console.log(tempPassword);
+
+  const [visible, setVisible] = useState(false);
+  const form = useForm({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: tempPassword || "",
+    },
+  });
+
+  async function onSubmit(values) {
+    // console.log(values);
+    await login(values);
+  }
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col items-center gap-2">
+              <Link
+                to="/"
+                className="flex flex-col items-center gap-2 font-medium"
+              >
+                <div className="flex size-8 items-center justify-center rounded-md">
+                  <Logo className="size-6" />
+                </div>
+                <span className="sr-only">Ovol Store</span>
+              </Link>
+              <h1 className="text-xl font-bold">Welcome to Ovol Store.</h1>
+              <div className="text-center text-sm">
+                Don&apos;t have an account?{" "}
+                <Link to="/signup" className="underline underline-offset-4">
+                  Sign up
+                </Link>
+              </div>
+            </div>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => {
+                    return (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            type="email"
+                            placeholder="m@example.com"
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    );
+                  }}
+                />
+              </div>
+              <div className="grid gap-3">
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => {
+                    return (
+                      <FormItem>
+                        <div className="flex items-center">
+                          <FormLabel>Password</FormLabel>
+                          <Link
+                            to="/forgot-password"
+                            className="ml-auto inline-block text-sm underline"
+                          >
+                            Forgot your password?
+                          </Link>
+                        </div>
+                        <FormControl>
+                          <div className="relative">
+                            <Input
+                              {...field}
+                              type={`${visible ? "text" : "password"}`}
+                              placeholder="enter your password"
+                            />
+                            {visible ? (
+                              <Eye
+                                onClick={() =>
+                                  setVisible((prevState) => !prevState)
+                                }
+                                className="absolute bottom-1.5 right-3 cursor-pointer"
+                              />
+                            ) : (
+                              <EyeOffIcon
+                                onClick={() =>
+                                  setVisible((prevState) => !prevState)
+                                }
+                                className="absolute bottom-1.5 right-3 cursor-pointer"
+                              />
+                            )}
+                          </div>
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    );
+                  }}
+                />
+              </div>
+              <Button type="submit" className="w-full" disabled={loading}>
+                {loading ? (
+                  <>
+                    <Loader2 className="animate-spin mr-1" />
+                    <span>Loading...</span>
+                  </>
+                ) : (
+                  "Login"
+                )}
+              </Button>
+            </div>
+            <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+              <span className="bg-background text-muted-foreground relative z-10 px-2">
+                Or
+              </span>
+            </div>
+            <div className="grid gap-4 sm:grid-cols-2">
+              <Button
+                variant="outline"
+                type="button"
+                className="w-full"
+                disabled
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <path
+                    d="M12.152 6.896c-.948 0-2.415-1.078-3.96-1.04-2.04.027-3.91 1.183-4.961 3.014-2.117 3.675-.546 9.103 1.519 12.09 1.013 1.454 2.208 3.09 3.792 3.039 1.52-.065 2.09-.987 3.935-.987 1.831 0 2.35.987 3.96.948 1.637-.026 2.676-1.48 3.676-2.948 1.156-1.688 1.636-3.325 1.662-3.415-.039-.013-3.182-1.221-3.22-4.857-.026-3.04 2.48-4.494 2.597-4.559-1.429-2.09-3.623-2.324-4.39-2.376-2-.156-3.675 1.09-4.61 1.09zM15.53 3.83c.843-1.012 1.4-2.427 1.245-3.83-1.207.052-2.662.805-3.532 1.818-.78.896-1.454 2.338-1.273 3.714 1.338.104 2.715-.688 3.559-1.701"
+                    fill="currentColor"
+                  />
+                </svg>
+                Continue with Apple
+              </Button>
+              <GoogleAuth />
+            </div>
+          </div>
+        </form>
+      </Form>
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        By clicking continue, you agree to our <TermsAndConditions /> and{" "}
+        <PrivacyPolicies />.
+      </div>
+    </div>
+  );
+}
