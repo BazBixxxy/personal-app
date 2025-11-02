dccbccde7b48ad509651bc179e75774c66421700
BazBixxxy <133676922+BazBixxxy@users.noreply.github.com>
2025-11-02 21:58:00 +0300

    Sure! Please provide the git diff so I can help you craft an appropriate commit message.

---
+++ src/app/dashboard/articles/add/hooks/useCreateArticle.js
@@ -0,0 +1,28 @@
+import articleApi from "@/services/api/articleApi";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+
+const useCreateAritcle = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  const createArticle = async (data) => {
+    try {
+      setLoading(true);
+      const res = await articleApi.createArticle(data);
+      const response = res.data;
+      toast.success("Article Created Successfully");
+      navigate("/dashboard/home");
+    } catch (error) {
+      toast.error(error.response.message);
+      console.error("Error creating article:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { loading, createArticle };
+};
+
+export default useCreateAritcle;
---
+++ src/services/api/articleApi.js
@@ -0,0 +1,19 @@
+const articleApi = {
+  createArticle: (data) => {
+    return instance.post("/articles", data);
+  },
+  updateArticle: (id, data) => {
+    return instance.patch(`/articles/${id}`, data);
+  },
+  fetchArticle: (id) => {
+    return instance.get(`/articles/${id}`);
+  },
+  deleteArticle: (id) => {
+    return instance.delete(`/articles/${id}`);
+  },
+  fetchArticles: (params) => {
+    return instance.get("/articles", { params });
+  },
+};
+
+export default articleApi;
